b3975d6b087b4b45ca630ba62c42a7024df15f0a
Michael Chimento <mchimento@ab.mpg.de>
2021-01-30 15:54:49 +0100

    Update TABLE_S1_S2.R

--- 4_create_figures/code/TABLE_S1_S2.R
+++ 4_create_figures/code/TABLE_S1_S2.R
@@ -16,7 +16,7 @@ load("../../2_Python_agent_based_models/data/df_TTC_m2.Rda")
 df = df_TTC_m2
 df$pop_size = as.factor(df$pop_size)
 M2_TTC_glm = glm(log(epoch+1) ~ graph*pop_size, family=gaussian(link="log"),data=df)
-stargazer(M1_TTC_glm,M2_TTC_glm,apply.coef= function(x) exp(x), summary = FALSE, digits=3, type="html", intercept.bottom = FALSE,single.row = TRUE, font.size = "small",report="vcstp*",out="../output/tableS1.html")
+stargazer(M1_TTC_glm,M2_TTC_glm,apply.coef= function(x) exp(x), summary = FALSE, digits=3, type="html", intercept.bottom = FALSE,single.row = TRUE, font.size = "small",report="vcstp*",out="../results/tableS1.html")
 
 #### Table S2 ####
 df = df_TTC_m1 %>% filter(graph!="full") %>% droplevels()
@@ -29,4 +29,4 @@ df = df_TTC_m2 %>% filter(graph!="full") %>% droplevels()
 df$pop_size = as.factor(df$pop_size)
 df$degree = as.factor(df$degree)
 M2_TTC_glm = glm(log(df$epoch+1) ~ graph*pop_size*degree, family=gaussian(link="log"),data=df)
-stargazer(M1_TTC_glm,M2_TTC_glm,apply.coef= function(x) exp(x),summary = FALSE, digits=3, type="html", intercept.bottom = FALSE,single.row = TRUE, font.size = "small",report="vcstp*",out="../output/tableS2.html")
+stargazer(M1_TTC_glm,M2_TTC_glm,apply.coef= function(x) exp(x),summary = FALSE, digits=3, type="html", intercept.bottom = FALSE,single.row = TRUE, font.size = "small",report="vcstp*",out="../results/tableS2.html")
